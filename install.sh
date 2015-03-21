d9120f788a02da008608404700f9b517eef01047
Stefan Roemer <sroemer@github.com>
2015-03-21 13:27:53 +0100

    added install.sh for creation of symblic links in the home directory

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+let rc=0
+if [ -e ~/.vimrc ]; then
+    echo "error: ~/.vimrc already exists"
+    rc=1
+fi
+if [ -e ~/.vim ]; then
+    echo "error: ~/.vim already exists"
+    rc=1
+fi
+if [ $rc == 0 ]; then
+    ln -sr ./vimrc ~/.vimrc && ln -sr ./vim ~/.vim
+    if [ $? != 0 ]; then
+        echo "error: creating symbolic links failed"
+    else
+        echo "symbolic links to srVimConfiguration successfully created"
+    fi
+fi
+exit $rc
+
